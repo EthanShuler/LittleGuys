01def6dee89d75bac4bd916e682f0bdc5ae02cb3
EthanShuler <13ethanshuler@gmail.com>
2023-10-21 21:15:16 -0700

    removed story

--- components/GuyCard/GuyCard.story.tsx
+++
@@ -1,8 +0,0 @@
-import { GuyCard } from './GuyCard';
-import myImage from './myImage.png';
-
-export default {
-  title: 'GuyCard',
-};
-
-export const Default = () => <GuyCard id={0} name="Test" image={myImage} />;
